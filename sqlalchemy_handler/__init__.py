bc32e7ead560bbd4ff8f0bbdf449ec9d8e35de37
erwanledoux <lawanledoux@gmail.com>
2019-09-20 17:57:30 +0200

    externalize-as-dict

--- sqlalchemy_handler/__init__.py
+++ sqlalchemy_handler/__init__.py
@@ -1,24 +1,3 @@
-from pprint import pprint
-from sqlalchemy import BigInteger,\
-                       Column
-
-from sqlalchemy_handler.find_or_create import FindOrCreate
-from sqlalchemy_handler.human_ids import humanize
-from sqlalchemy_handler.save import Save
+from sqlalchemy_handler.api_handler import ApiHandler
 
 __version__ = "0.0.3"
-
-class Handler(Save, FindOrCreate):
-
-    id = Column(BigInteger,
-                primary_key=True,
-                autoincrement=True)
-
-    def dump(self):
-        pprint(vars(self))
-
-    def __repr__(self):
-        self_id = "unsaved"\
-               if self.id is None\
-               else str(self.id) + "/" + humanize(self.id)
-        return '<%s #%s>' % (self.__class__.__name__, self_id)
---
+++ sqlalchemy_handler/api_handler.py
@@ -0,0 +1,22 @@
+from pprint import pprint
+from sqlalchemy import BigInteger,\
+                       Column
+
+from sqlalchemy_handler.find_or_create import FindOrCreate
+from sqlalchemy_handler.human_ids import humanize
+from sqlalchemy_handler.save import Save
+
+class ApiHandler(Save, FindOrCreate):
+
+    id = Column(BigInteger,
+                primary_key=True,
+                autoincrement=True)
+
+    def dump(self):
+        pprint(vars(self))
+
+    def __repr__(self):
+        self_id = "unsaved"\
+               if self.id is None\
+               else str(self.id) + "/" + humanize(self.id)
+        return '<%s #%s>' % (self.__class__.__name__, self_id)
