a6a15f012f26dc2e0324f3e4f0612bea45bdb1e0
erwanledoux <lawanledoux@gmail.com>
2019-09-21 16:59:55 +0200

    export ApiErrors

--- sqlalchemy_api_handler/__init__.py
+++ sqlalchemy_api_handler/__init__.py
@@ -1,3 +1,4 @@
+from sqlalchemy_api_handler.api_errors import ApiErrors
 from sqlalchemy_api_handler.api_handler import ApiHandler
 from sqlalchemy_api_handler.serialization.as_dict import as_dict
 from sqlalchemy_api_handler.utils.human_ids import humanize, dehumanize
