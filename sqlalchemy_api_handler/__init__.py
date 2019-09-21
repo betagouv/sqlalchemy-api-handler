9f74a14bb2c9732b0b7236263a47836538607509
erwanledoux <lawanledoux@gmail.com>
2019-09-21 16:58:34 +0200

    export directly as_dict and humanize dehumanize

--- sqlalchemy_api_handler/__init__.py
+++ sqlalchemy_api_handler/__init__.py
@@ -1,3 +1,5 @@
 from sqlalchemy_api_handler.api_handler import ApiHandler
+from sqlalchemy_api_handler.serialization.as_dict import as_dict
+from sqlalchemy_api_handler.utils.human_ids import humanize, dehumanize
 
 __version__ = "0.0.3"
