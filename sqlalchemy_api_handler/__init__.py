bdb5ee27f0d22282fbdca850667436a4a05ea66f
erwanledoux <lawanledoux@gmail.com>
2019-09-21 16:58:48 +0200

    0.0.4

--- sqlalchemy_api_handler/__init__.py
+++ sqlalchemy_api_handler/__init__.py
@@ -2,4 +2,4 @@ from sqlalchemy_api_handler.api_handler import ApiHandler
 from sqlalchemy_api_handler.serialization.as_dict import as_dict
 from sqlalchemy_api_handler.utils.human_ids import humanize, dehumanize
 
-__version__ = "0.0.3"
+__version__ = "0.0.4"
