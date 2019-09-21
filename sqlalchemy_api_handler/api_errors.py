53273aa811408211a5ddea3f384da80ce8828542
erwanledoux <lawanledoux@gmail.com>
2019-09-21 19:00:54 +0200

    readded check min lenght

--- sqlalchemy_api_handler/api_errors.py
+++ sqlalchemy_api_handler/api_errors.py
@@ -17,12 +17,20 @@ class ApiErrors(Exception):
         else:
             self.add_error(field, 'format is invalid')
 
+    def check_email(self, field, value):
+        if not "@" in value:
+            self.add_error(field, 'L\'e-mail doit contenir un @.')
+            
     def check_float(self, field, value):
         if isinstance(value, float) or \
            (isinstance(value, (str, unicode)) and re.search('^\d+(\.\d*|)$', value)):
             return True
         else:
             self.add_error(field, 'value must be a number.')
+
+    def check_min_length(self, field, value, length):
+        if len(value) < length:
+            self.add_error(field, 'value should be long at least of ' + str(length) + ' characters.')
 
     def maybe_raise(self):
         if len(self.errors) > 0:
