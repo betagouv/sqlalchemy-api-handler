543b9be1e6dafa0bacfc360b121e036411f0b104
erwanledoux <lawanledoux@gmail.com>
2019-09-21 16:44:10 +0200

    fixed import of submodules

--- sqlalchemy_api_handler/__init__.py
+++ sqlalchemy_api_handler/__init__.py
@@ -1,3 +1,7 @@
 from sqlalchemy_api_handler.api_handler import ApiHandler
+from sqlalchemy_api_handler import bases
+from sqlalchemy_api_handler import mixins
+from sqlalchemy_api_handler import serialization
+from sqlalchemy_api_handler import utils
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
