727aed7398647a29586e88071d07b963d8ae7152
erwanledoux <lawanledoux@gmail.com>
2019-09-23 16:41:59 +0200

    Revert "1.1.2"
    This reverts commit 60cfb1a307ed2791a52afc370d84261847a00861.

--- sqlalchemy_api_handler/__init__.py
+++ sqlalchemy_api_handler/__init__.py
@@ -5,4 +5,4 @@ from sqlalchemy_api_handler.utils.human_ids import humanize, dehumanize
 from sqlalchemy_api_handler.utils.listify import listify
 from sqlalchemy_api_handler.utils.load_or_404 import load_or_404
 
-__version__ = "0.1.2"
+__version__ = "0.1.1"
