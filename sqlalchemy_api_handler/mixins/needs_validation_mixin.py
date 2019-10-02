97768b272ca3979bf95004a53bf8f7453a9d0000
erwanledoux <lawanledoux@gmail.com>
2019-10-02 16:20:57 +0200

    added needs validation mixin

---
+++ sqlalchemy_api_handler/mixins/needs_validation_mixin.py
@@ -0,0 +1,20 @@
+import secrets
+
+from sqlalchemy import Column, String
+
+
+class NeedsValidationMixin(object):
+    validationToken = Column(String(27),
+                             unique=True,
+                             nullable=True)
+
+    def generate_validation_token(self):
+        self.validationToken = secrets.token_urlsafe(20)
+
+    @property
+    def queryValidated(self):
+        return self.query.filter(NeedsValidationMixin.validationToken == None)
+
+    @property
+    def isValidated(self):
+        return self.validationToken is None
