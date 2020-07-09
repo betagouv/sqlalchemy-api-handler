aa7833e87747d06cb457ed5d7b775b84d9134a35
Eledoux <lawanledoux@gmail.com>
2020-07-09 11:13:26 +0200

    added model from table name

--- sqlalchemy_api_handler/bases/accessor.py
+++ sqlalchemy_api_handler/bases/accessor.py
@@ -1,3 +1,6 @@
+import inflect
+
+
 class Accessor():
     @classmethod
     def get_db(cls):
@@ -6,3 +9,15 @@ class Accessor():
     @classmethod
     def set_db(cls, db):
         Accessor.db = db
+
+    @classmethod
+    def model_with_table_name(cls, table_name):
+        for model in Accessor.get_db().Model._decl_class_registry.values():
+            if not hasattr(model, '__table__'):
+                continue
+            if model.__tablename__ == table_name:
+                return model
+
+    @classmethod
+    def model_with_plural_name(cls, plural_name):
+        return Accessor.model_with_table_name(inflect.engine().singular_noun(plural_name))
