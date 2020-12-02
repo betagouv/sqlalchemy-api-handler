2c92dc5d4ae2bef1d0581e437f388dad52d9f708
Eledoux <lawanledoux@gmail.com>
2020-12-02 22:22:13 +0100

    fix dateCreated

--- tests/api_handler_test.py
+++ tests/api_handler_test.py
@@ -2,23 +2,25 @@ import pytest
 
 from sqlalchemy_api_handler import ApiHandler
 from tests.conftest import with_delete
-from api.models.offer import Offer
+from api.models.user import User
 
 
 class SaveTest():
     @with_delete
-    def test_save_offer(self, app):
+    def test_save_user(self, app):
         # given
-        offer_dict = {
-            'name': 'bar',
-            'type': 'fee'
+        user_dict = {
+            'email': 'marx.foo@plop.fr',
+            'firstName': 'Marx',
+            'lastName': 'Foo',
+            'publicName': 'Marx Foo'
         }
-        offer = Offer(**offer_dict)
+        user = User(**user_dict)
 
         # when
-        ApiHandler.save(offer)
+        ApiHandler.save(user)
 
         # then
-        saved_offer = Offer.query.first()
-        for (key, value) in offer_dict.items():
-            assert getattr(saved_offer, key) == value
+        saved_user = User.query.first()
+        for (key, value) in user_dict.items():
+            assert getattr(saved_user, key) == value
