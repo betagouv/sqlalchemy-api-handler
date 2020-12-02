427d57dc74f2dee93fd2f1af8cf4b0ca570b6b8f
Eledoux <lawanledoux@gmail.com>
2020-12-02 23:01:08 +0100

    try but no

--- api/utils/database.py
+++ api/utils/database.py
@@ -21,6 +21,7 @@ def delete():
 def create_activity_and_transaction_tables():
     orm.configure_mappers()
     Activity = ApiHandler.get_activity()
+    #ApiHandler.model_from_name('User').__table__.create(db.session.get_bind())
     Activity.transaction.mapper.class_.__table__.create(db.session.get_bind())
     Activity.__table__.create(db.session.get_bind())
 
@@ -28,6 +29,15 @@ def create_activity_and_transaction_tables():
 def create():
     logger.info('Create all the database...')
     create_activity_and_transaction_tables()
+    '''
+    tables_except_user = [
+        table
+        for (table_name, table) in db.metadata.tables.items()
+        if table_name != ApiHandler.model_from_name('User').__tablename__
+    ]
+    db.metadata.create_all(db.engine,
+                           tables=tables_except_user)
+    '''
     db.create_all()
     db.session.commit()
     logger.info('Create all the database...Done.')
